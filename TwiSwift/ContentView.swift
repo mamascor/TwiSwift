6260bcb0b2051314dc6ef25fa6e911fb0233b6c6
Marco Mascorro <marco.mascorro@gmail.com>
2023-06-10 23:01:11 -0500

    Initial Commit

---
+++ TwiSwift/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  TwiSwift
+//
+//  Created by Marco Mascorro on 6/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TwiSwift/TwiSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwiSwiftApp.swift
+//  TwiSwift
+//
+//  Created by Marco Mascorro on 6/10/23.
+//
+
+import SwiftUI
+
+@main
+struct TwiSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
